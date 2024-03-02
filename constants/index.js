97c835342a81da4f642cb3ae63e3f9fc99ab2605
Sabhyaa <pradhansabhyaa2005@gmail.com>
2024-03-02 14:48:08 +0545

    first commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Paragraph 1',
+    subtitle:
+        'Subtitle 1',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Paragraph 2',
+    subtitle:
+        'subtitle 2',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Paragraph 3',
+    subtitle:
+        'Subtitle 3',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
